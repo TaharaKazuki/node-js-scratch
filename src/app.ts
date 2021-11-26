b60162e54556decdeae562ecd495eeee17d61829
Tahara Kazuki <ssp0727.lnc@gmail.com>
2021-11-26 22:45:34 +0900

    update

---
+++ src/app.ts
@@ -0,0 +1,36 @@
+import express, { Application } from 'express'
+import mongoose from 'mongoose'
+import compression from 'compression'
+import cors from 'cors'
+import morgan from 'morgan'
+import helmet from 'helmet'
+
+import Controller from '@utils/interfaces/controller.interface'
+
+import ErrorMiddleware from '@/middleware/error.middleware'
+
+class App {
+  public express: Application
+  public port: number
+
+  constructor(controllers: Controller[], port: number) {
+    this.express = express()
+    this.port = port
+
+    this.initialiseDatabaseConnection()
+    this.initialiseMiddleware()
+    this.initialiseControllers(controllers)
+    this.initialiseErrorHandling()
+  }
+
+  private initialiseMiddleware(): void {
+    this.express.use(helmet())
+    this.express.use(cors())
+    this.express.use(morgan('dev'))
+    this.express.use(express.json())
+    this.express.use(express.urlencoded({ extended: false }))
+    this.express.use(compression())
+  }
+
+  private initialiseControllers(controllers: Controller[]): void {}
+}
